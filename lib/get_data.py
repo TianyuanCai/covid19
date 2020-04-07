12fd68dfe2c9887a73118bc11755d6059bd09f5d
Tianyuan Cai <tycai47@gmail.com>
2020-04-07 19:13:11 -0400

    weather and restaurant data main

--- lib/get_data.py
+++ lib/get_data.py
@@ -36,31 +36,50 @@ def get_restaurant_data(zip_code):
         tmp_df = tmp_df[(tmp_df['location.zip_code'] == zip_code) & (tmp_df['is_closed'].astype(int) == 0)]
         tmp_df = tmp_df[['name', 'review_count', 'categories', 'rating', 'transactions', 'price', 'location.zip_code']]
 
-        df = df.append(tmp_df)
+        restaurant_df = restaurant_df.append(tmp_df)
         n_record = min(n_record, json.loads(r.text)['total'])
 
     if os.path.exists(restaurant_data_file):
-        df.to_csv(restaurant_data_file, index=False, header=False, mode='a')
+        restaurant_df.to_csv(restaurant_data_file, index=False, header=False, mode='a')
     else:
-        df.to_csv(restaurant_data_file, index=False)
+        restaurant_df.to_csv(restaurant_data_file, index=False)
 
 
-def get_weather_data(zip_code):
+def get_weather_data(zip_code, date):
     """
     Download weather data to folder, append if file aleady exists
     https://developer.weathersource.com/tools/postman-collection-onpoint-api/
     covid19 Research access
 
     :param zip_code: str, Example '94118'
+    :param date: str, Example '2020-02-01'
     :return:
     """
     api_key = '84663a2b5a93171ddb2a'
     url = f'https://api.weathersource.com/v1/{api_key}/postal_codes/{zip_code},' \
-          f'us/history.json?period=day&timestamp_eq=2018-01-02'
+          f'us/history.json?period=day&timestamp_eq={date}'
     r = requests.get(url)
-    df = pd.json_normalize(json.loads(r.text))
+    weather_df = pd.json_normalize(json.loads(r.text))
 
     if os.path.exists(weather_data_file):
-        df.to_csv(weather_data_file, index=False, header=False, mode='a')
+        weather_df.to_csv(weather_data_file, index=False, header=False, mode='a')
     else:
-        df.to_csv(weather_data_file, index=False)
+        weather_df.to_csv(weather_data_file, index=False)
+
+
+if __name__ == '__main__':
+    nyt_df = pd.read_csv('data/nytimes_covid19_data/20200330_us-counties.csv')
+
+    zip_df = pd.read_csv('data/raw/uszips.csv')  # https://simplemaps.com/data/us-zips
+    zip_df['zip'] = zip_df['zip'].astype(str).str.pad(width=5, side='left', fillchar='0')
+
+    # download restaurant and weather data to raw folder
+    for z in set(zip_df['zip']):
+        get_restaurant_data(z)
+
+        for d in set(nyt_df['date']):
+            get_weather_data(z, d)
+
+    # todo merge to nyt data after aggregating by zip codes
+    county_zips = pd.DataFrame(zip_df.groupby(['county_name'])['zip'].apply(list)).reset_index()
+    df = nyt_df.merge(county_zips, how='left', left_on='county', right_on='county_name')
