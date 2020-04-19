811485d223d90ec866d8522e524eb0832b980ee2
Tianyuan Cai <tycai47@gmail.com>
2020-04-19 00:58:58 -0400

    updated weather and restaurant data

--- lib/get_data.py
+++ lib/get_data.py
@@ -3,6 +3,7 @@ import requests
 import os
 import time
 import datetime
+import ast
 
 import pandas as pd
 import numpy as np
@@ -113,6 +114,21 @@ if __name__ == '__main__':
 
     # append all zip index to nyt data frame
     df = nyt_df.merge(zip_df, how='left', left_on='fips', right_on='county_fips_all')
+
+    # restaurant planning
+    restaurant_df['transactions'] = restaurant_df['transactions'].apply(lambda x: '|'.join(list(ast.literal_eval(x))))
+    restaurant_dummies = pd.concat([pd.get_dummies(restaurant_df['price'], drop_first=True, prefix='price'),
+                                    pd.get_dummies(restaurant_df['transactions'], drop_first=True,
+                                                   prefix='transactions')], axis=1)
+    restaurant_mean = restaurant_df.groupby('location.zip_code')['review_count', 'rating'].mean().reset_index()
+    restaurant_df = pd.concat([restaurant_mean, restaurant_dummies], axis=1)
+
+    # weather join by datetime and weather_df['timestamp']
+    weather_df['timestamp'] = weather_df['timestamp'].str.slice(stop=10)
+
+    # also join by postal_code
+
+    # average reviews, count of bars, etc.
 
     # aggregate weather and restaurant data from fip to zip level
 
