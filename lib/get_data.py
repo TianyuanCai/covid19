d874cf1f295fcd3b0f76049b6d3c933ef94da5a2
Tianyuan Cai <tycai47@gmail.com>
2020-04-20 00:19:07 -0400

    usable final data set w weather info

--- lib/get_data.py
+++ lib/get_data.py
@@ -93,9 +93,9 @@ def get_zip_mapping():
 
 
 def aggregate_data():
-    restaurant_df = pd.read_csv(restaurant_data_file)
-    restaurant_df['location.zip_code'] = restaurant_df['location.zip_code'].astype(str).str.pad(width=5, side='left',
-                                                                                                fillchar='0')
+    # restaurant_df = pd.read_csv(restaurant_data_file)
+    # restaurant_df['location.zip_code'] = restaurant_df['location.zip_code'].astype(str).str.pad(width=5, side='left',
+    #                                                                                             fillchar='0')
 
     weather_df = pd.read_csv(weather_data_file)
     weather_df['postal_code'] = weather_df['postal_code'].astype(str).str.pad(width=5, side='left', fillchar='0')
@@ -105,22 +105,22 @@ def aggregate_data():
 
     zip_df = get_zip_mapping()
 
-    # restaurant
-    # todo: nice to haves: feature eng from categories: count of bars, cafes, etc.
-    restaurant_df['transactions'] = restaurant_df['transactions'].apply(lambda x: '|'.join(list(ast.literal_eval(x))))
-    restaurant_dummies = pd.concat([pd.get_dummies(restaurant_df['price'], drop_first=True, prefix='price'),
-                                    pd.get_dummies(restaurant_df['transactions'], drop_first=True,
-                                                   prefix='transactions'),
-                                    restaurant_df['location.zip_code']], axis=1)
-    restaurant_dummies = restaurant_dummies.groupby('location.zip_code').sum().reset_index().add_prefix('sum_')
-    restaurant_mean = restaurant_df.groupby('location.zip_code')[
-        ['review_count', 'rating']].mean().reset_index().add_prefix('mean_')
-
-    restaurant_df = pd.merge(restaurant_mean, restaurant_dummies, left_on='mean_location.zip_code',
-                             right_on='sum_location.zip_code', validate='1:1')
-    restaurant_df = pd.merge(restaurant_df, zip_df, left_on='sum_location.zip_code', right_on='zip', how='left')
-    restaurant_df = restaurant_df.groupby('county_fips_all').mean().reset_index()  # restaurant data on zip level
-    restaurant_df = restaurant_df.drop(restaurant_df.filter(regex='zip', axis=1).columns)
+    # # restaurant
+    # # todo: nice to haves: feature eng from categories: count of bars, cafes, etc.
+    # restaurant_df['transactions'] = restaurant_df['transactions'].apply(lambda x: '|'.join(list(ast.literal_eval(x))))
+    # restaurant_dummies = pd.concat([pd.get_dummies(restaurant_df['price'], drop_first=True, prefix='price'),
+    #                                 pd.get_dummies(restaurant_df['transactions'], drop_first=True,
+    #                                                prefix='transactions'),
+    #                                 restaurant_df['location.zip_code']], axis=1)
+    # restaurant_dummies = restaurant_dummies.groupby('location.zip_code').sum().reset_index().add_prefix('sum_')
+    # restaurant_mean = restaurant_df.groupby('location.zip_code')[
+    #     ['review_count', 'rating']].mean().reset_index().add_prefix('mean_')
+    #
+    # restaurant_df = pd.merge(restaurant_mean, restaurant_dummies, left_on='mean_location.zip_code',
+    #                          right_on='sum_location.zip_code', validate='1:1')
+    # restaurant_df = pd.merge(restaurant_df, zip_df, left_on='sum_location.zip_code', right_on='zip', how='left')
+    # restaurant_df = restaurant_df.groupby('county_fips_all').mean().reset_index()  # restaurant data on zip level
+    # restaurant_df = restaurant_df.drop(restaurant_df.filter(regex='zip', axis=1).columns)
 
     # weather
     weather_df['timestamp'] = weather_df['timestamp'].str.slice(stop=10)
@@ -130,7 +130,7 @@ def aggregate_data():
 
     # join data
     df = pd.merge(nyt_df, weather_df, left_on=['fips', 'date'], right_on=['county_fips_all', 'timestamp'], how='inner')
-    df = pd.merge(df, restaurant_df, left_on='fips', right_on='county_fips_all', how='inner')
+    # df = pd.merge(df, restaurant_df, left_on='fips', right_on='county_fips_all', how='inner')
 
     return df
 
