9950cfcffddd5ea95d700538b981151393486935
Tianyuan Cai <tycai47@gmail.com>
2020-04-27 20:44:18 -0400

    calculation update

--- lib/get_data.py
+++ lib/get_data.py
@@ -127,11 +127,11 @@ def get_model_data(date_range=(0, 14), pred_day=21):
 
     # filter for training and testing dates
     df_x = df[df['days_since_10_cases'].between(date_range[0], date_range[1])]
-    df_x = df_x.groupby(['state', 'county', 'fips']).mean().reset_index()
+    df_x = df_x.groupby(['state', 'county', 'fips']).mean().reset_index()  # todo play with granularity
     df_x = df_x.drop(['days_since_10_cases'], axis=1)
 
     # get change rate since last training date
-    df_y = df[df['days_since_10_cases'].isin([date_range[1] + 1, pred_day])][
+    df_y = df[df['days_since_10_cases'].isin([date_range[1], pred_day])][
         ['fips', 'cases', 'deaths', 'days_since_10_cases']]
     df_y = df_y.sort_values(['fips', 'days_since_10_cases']).reset_index(drop=True)
     day_idx = np.where(df_y['days_since_10_cases'] == pred_day)[0]
@@ -142,10 +142,6 @@ def get_model_data(date_range=(0, 14), pred_day=21):
     df_x = df_x.merge(df_y_delta, left_on='fips', right_on=f'day_{pred_day}_delta_fips', how='inner')
 
     return df_x
-
-
-# for i in [(7, 14), (14, 21), (14, 16)]:
-#     print(len(get_model_data(date_range=(0, i[0]), pred_day=i[1])))
 
 
 if __name__ == '__main__':
